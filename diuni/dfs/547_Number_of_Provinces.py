ba5a17ce64e8355cd80de56cdb490f15a22d442e
Jiyun Park <wldbs0507@kaist.ac.kr>
2022-03-17 22:44:47 +0900

    Solve dfs & binary search problems

--- diuni/dfs/547_Number_of_Provinces.py
+++ diuni/dfs/547_Number_of_Provinces.py
@@ -4,14 +4,22 @@ class Solution(object):
         :type isConnected: List[List[int]]
         :rtype: int
         """
-        
+
+        # 2022-03-13
+
+        # First Trial 
+
+        # Time Complexity: O(n)??? /   Runtime: faster than 49.33%
+        # Space Complexity: O(n) /   Memory Usage: less than 13.89% 
         
         def dfs(idx, connections, visited):
-            # print("i:", idx, isVisited)
+        # def dfs(idx, isConnected, visited):
             visited[idx] = True
             for i, con in enumerate(connections):
+            # for i, con in enumerate(isConnected[idx]):
                 if con == 1 and visited[i] == False:
                     dfs(i, isConnected[i], visited)
+                    # dfs(i, isConnected, visited)
             return visited
         
         province = 1
@@ -20,6 +28,7 @@ class Solution(object):
         
         while True:
             visited = dfs(start, isConnected[start], visited)
+            # visited = dfs(start, isConnected, visited)
             if False in visited:
                 start = visited.index(False)
                 province += 1
